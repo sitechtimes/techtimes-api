9ff46e754c948d64407d9dd571f61c74779b430f
edis <edis@gonuler.com>
2021-02-17 19:33:29 -0500

    add user model w/ mongo setup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -13,10 +14,14 @@ router.post('/api/auth/signup',
             .trim()
             .isLength({min: 8, max: 16})
             .withMessage('Password must be between 8 and 16 characters')
-    ], (req: Request, res: Response) => {
+    ], async (req: Request, res: Response) => {
 
     const { email, password } = req.body;
-    console.log(email, password, router);
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    return res.status(201).send(user)
 });
 
 export { router as signupRouter } ;
