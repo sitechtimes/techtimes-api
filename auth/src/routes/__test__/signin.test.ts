57609e8555b55dd247605298e16c75eb4e16c7b7
edis <edis@gonuler.com>
2021-02-18 11:30:07 -0500

    add test for signin route

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,46 @@
+import request from 'supertest';
+import {app} from "../../app";
+
+it('returns a 400 when signin is attempted with an email does not exist', async () => {
+    await request(app).post('/api/auth/signin')
+        .send({
+            email: 'doesnotexist@sitechhs.com',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('returns a 400 when signin is attempted with an incorrect password', async () => {
+    await request(app).post('/api/auth/signup')
+        .send({
+            email: 'test@sitechhs.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app).post('/api/auth/signin')
+        .send({
+            email: 'test@sitechhs.com',
+            password: 'wrongpassword'
+        })
+        .expect(400);
+});
+
+it('returns a cookie when valid credentials are given', async () => {
+    await request(app).post('/api/auth/signup')
+        .send({
+            email: 'test@sitechhs.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    const response = await request(app).post('/api/auth/signin')
+        .send({
+            email: 'test@sitechhs.com',
+            password: 'password'
+        })
+        .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+});
+
