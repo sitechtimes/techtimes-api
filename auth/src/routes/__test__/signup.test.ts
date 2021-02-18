3fc7fd1ac9dfaf044407c538df6bac8b01ae2bd7
edis <edis@gonuler.com>
2021-02-18 11:21:19 -0500

    add tests for signup route

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -9,3 +9,63 @@ it('returns a 201 on successful signup', async () => {
         })
         .expect(201);
 });
+
+it('returns a 400 with an invalid formatted email', async () => {
+    return request(app).post('/api/auth/signup')
+        .send({
+            email: 'test',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('returns a 400 with an invalid sitechhs email', async () => {
+    return request(app).post('/api/auth/signup')
+        .send({
+            email: 'test@gmail.com',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('returns a 400 with a missing email & password', async () => {
+    await request(app).post('/api/auth/signup')
+        .send({
+            email: 'test@sitechhs.com',
+        })
+        .expect(400);
+
+    await request(app).post('/api/auth/signup')
+        .send({
+            password: 'password',
+        })
+        .expect(400);
+});
+
+it('returns a 400 if signup with existing email is attempted', async () => {
+    await request(app).post('/api/auth/signup')
+        .send({
+            email: 'test@sitechhs.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app).post('/api/auth/signup')
+        .send({
+            email: 'test@sitechhs.com',
+            password: 'password',
+        })
+        .expect(400);
+});
+
+
+it('sets a cookie after a succesful signup', async () => {
+    const response =  await request(app).post('/api/auth/signup')
+        .send({
+            email: 'test@sitechhs.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+});
