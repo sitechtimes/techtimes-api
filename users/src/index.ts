1c209db0d4ed2f9cacc880c071e1b46a0f62cff0
edis <edis@gonuler.com>
2021-02-23 15:08:22 -0500

    add users express app

--- users/src/index.ts
+++ users/src/index.ts
@@ -1,12 +1,33 @@
-import express, { Request, Response } from 'express';
-import 'express-async-errors';
+import { app } from "./app";
+import mongoose from 'mongoose';
+import env from 'dotenv';
 
-const app = express();
+env.config({ path: `${__dirname}/../../.env.development`});
 
-app.get('/', (req: Request, res: Response) => {
-    res.send({});
-});
+const start = async  () => {
+    if (!process.env.JWT_KEY){
+        throw new Error('JWT_KEY must be defined')
+    }
 
-app.listen(3001, () => {
-    console.log("Listening on port 3001");
-});
+    if (!process.env.MONGO_URI){
+        throw new Error('MONGO_URI must be defined')
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI!, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log("Connected to mongodb")
+
+        app.listen(3001, () => {
+            console.log("Listening on port 3001");
+        });
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+start();
