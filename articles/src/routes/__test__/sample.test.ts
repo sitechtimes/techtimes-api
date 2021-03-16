66274a529cbf83d2798a9f079932dc5080bd0a97
edis <edis@gonuler.com>
2021-03-16 11:45:50 -0400

    update articles sample test

--- articles/src/routes/__test__/sample.test.ts
+++ articles/src/routes/__test__/sample.test.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import request from 'supertest';
 import {app} from '../../app';
 
@@ -6,13 +5,4 @@ it('return 200 for sample articles route', async () => {
     await request(app).get('/api/articles')
         .send()
         .expect(200);
-=======
-import request from "supertest";
-import { app } from "../../app";
-
-it('returns 200 when users tries to get sample route', async () => {
-    await request(app).get('/api/articles')
-        .send()
-        .expect(200)
->>>>>>> 4ba4ffe3a06a262f951fbb044a68e653bbb68614
 });
