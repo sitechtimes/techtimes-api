4457af7d67ea62a36f15be64bf15e625ebac0c33
Edis Gonuler <edis@gonuler.com>
2021-05-28 11:08:43 -0400

    add scripts to folder

---
+++ scripts/npm-install.sh
@@ -0,0 +1,8 @@
+install_deps() {
+  cd "../$1" && npm install && npm update && cd ../../ && echo "✅ Successfully installed npm packages in $1" || echo "Failed";
+}
+
+install_deps users
+install_deps auth
+install_deps cms
+
